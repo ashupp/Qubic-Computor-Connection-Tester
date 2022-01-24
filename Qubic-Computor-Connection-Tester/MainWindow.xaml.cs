b5f9f7ac8a9c968387a44e3b5d1f885b652a892b
Alexander Hupp <ahupp@ashnet.de>
2022-01-24 02:06:31 +0100

    First version

---
+++ Qubic-Computor-Connection-Tester/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SimpleTcp;
+
+namespace Qubic_Computor_Connection_Tester
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SimpleTcpClient _client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _client = new SimpleTcpClient(IpPortTextBox.Text);
+                _client.Settings.ConnectTimeoutMs = 2500;
+                _client.Events.Connected += Connected;
+                _client.Connect();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Could not connect: Check IP/Port: " + exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                StatusTextBox.Text = "Could not connect: " + exception.Message;
+            }
+            
+        }
+
+        private void Connected(object sender, ConnectionEventArgs e)
+        {
+            Console.WriteLine("Connected: " + e.IpPort + " - " + e.Reason);
+            StatusTextBox.Text = "Success: Connected. Now check Computor app for CONNECTED - Message";
+
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                _client.Events.Connected -= Connected;
+                _client.Disconnect();
+            }));
+            
+        }
+    }
+}
---
+++ Qubic-Computor-Connection-Tester/ObfuscationSettings.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: Obfuscation(Feature = "embed SimpleTcp.dll", Exclude = false)]
